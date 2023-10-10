8417e9ec24e6ff1080566b0b4584c193ca5a9cca
Ihya217 <ihyaulumuddin217@gmail.com>
2023-10-10 16:21:18 +0800

    nah ini

--- src/App.js
+++ src/App.js
@@ -53,7 +53,7 @@ const App = () => {
     <div className="fontFamily: 'Araboto Medium, sans-serif' flex">
       {/* Navbar */}
       <nav className="flex flex-col fixed top-0 left-0 h-full bg-transparent text-white mt-20 ps-1">
-  <ul className="ms-4 space-y-14 sm:space-y-14 md:space-y-20">
+  <ul className="ms-4 space-y-14 sm:space-y-14 md:space-y-14">
     <li className="flex flex-row transform -rotate-90 origin-bottom mb-4 justify-center">
       <a
         href="#home"
@@ -117,11 +117,11 @@ const App = () => {
 </nav>
 
       {/* Content */}
-      <div className="flex-grow bg-cover bg-fixed bg-no-repeat ps-20 pe-10"
+      <div className="flex-grow bg-cover bg-fixed bg-no-repeat px-20 "
          style={{ backgroundImage: 'url("/read-me/img/bgss.jpg")' }}>
         {/* Home Section */}
         <section id="home" className="min-h-screen flex items-center mx-auto ">
-      <div className="container mx-auto flex flex-col md:flex-row text-left md:text-left">
+      <div className="container mx-auto flex flex-col md:text-center lg:text-center">
         <div>
         <p className="text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-semibold text-white mb-4 font-mono">
   M. Ihya Ulumuddin
@@ -158,59 +158,58 @@ const App = () => {
 
 
     <section id="exp" className="min-h-screen flex items-center justify-center">
-      <div className='container mx-auto row'>
-        {/* Achievements */}
-        <div className="container-about mx-auto flex flex-col md:flex-row text-center mb-20">
-          <img
-            src="./img/bg.jpg"
-            alt="Foto Anda"
-            className="w-full md:w-1/2 rounded-xl mx-auto mb-8 md:mb-0 me-5"
-          />
-          <div className="w-full md:w-1/2 mx-auto ps-1 text-center md:text-left">
-  <h1 className="text-3xl md:text-4xl lg:text-5xl font-semibold font-mono text-white mb-4 md:mb-6">Achievements</h1>
-  <ul className="text-lg md:text-base text-white custom-tracking space-y-3 sm:space-y-4 md:space-y-4">
-    <li className="text-base md:text-lg lg:text-base">- 8 Times Got First Rank in Elementary School</li>
-    <li className="text-base md:text-lg lg:text-base">- Top 8 Student In Middle School</li>
-    <li className="text-base md:text-lg lg:text-base">- Runner-Up Of Genshin Impact Tournament</li>
-    <li className="text-base md:text-lg lg:text-base">- Design Winner in Class Meeting</li>
-    <li className="text-base md:text-lg lg:text-base">- Futsal Runner Up in Class Meeting</li>
-    <li className="text-base md:text-lg lg:text-base">- Band Winner in Class Meeting</li>
-  </ul>
-</div>
-        </div>
+  <div className='container mx-auto row'>
 
-        {/* Experience */}
-        <div className="container-about mx-auto flex flex-col md:flex-row text-center">
-          <img
-            src="./img/bg.jpg"
-            alt="Foto Anda"
-            className="w-full md:w-1/2 rounded-xl mx-auto mb-8 md:mb-0 me-5"
-          />
+    {/* Achievements */}
+    <div className="container-about mx-auto flex flex-col md:flex-row text-center mb-20">
+      <img
+        src="./img/bg.jpg"
+        alt="Foto Anda"
+        className="w-full md:w-1/2 lg:w-1/3 rounded-xl mx-auto mb-8 md:mb-0 md:me-5"
+      />
+      <div className="w-full md:w-1/2 lg:w-2/3 mx-auto ps-1 text-left md:text-left">
+        <h1 className="text-3xl md:text-4xl lg:text-5xl font-semibold font-mono text-white mb-4 md:mb-6">Achievements</h1>
+        <ul className="text-lg md:text-base text-white custom-tracking space-y-2 sm:space-y-4 md:space-y-4 list-disc">
+          <li className="text-base md:text-lg lg:text-base"> 8 Times Got First Rank in Elementary School</li>
+          <li className="text-base md:text-lg lg:text-base"> Top 8 Student In Middle School</li>
+          <li className="text-base md:text-lg lg:text-base"> Runner-Up Of Genshin Impact Tournament</li>
+          <li className="text-base md:text-lg lg:text-base"> Design Winner in Class Meeting</li>
+          <li className="text-base md:text-lg lg:text-base"> Futsal Runner Up in Class Meeting</li>
+          <li className="text-base md:text-lg lg:text-base"> Band Winner in Class Meeting</li>
+        </ul>
+      </div>
+    </div>
 
-<div className="w-full md:w-1/2 mx-auto ps-1 text-center md:text-left">
-  <h1 className="text-3xl md:text-4xl lg:text-5xl font-semibold font-mono text-white mb-4 md:mb-6">Experience</h1>
-  <ul className="text-lg md:text-base text-white custom-tracking space-y-3 sm:space-y-4 md:space-y-4">
-    <li className="text-base md:text-lg lg:text-base">- 8 Times Got First Rank in Elementary School</li>
-    <li className="text-base md:text-lg lg:text-base">- Top 8 Student In Middle School</li>
-    <li className="text-base md:text-lg lg:text-base">- Runner-Up Of Genshin Impact Tournament</li>
-    <li className="text-base md:text-lg lg:text-base">- Design Winner in Class Meeting</li>
-    <li className="text-base md:text-lg lg:text-base">- Futsal Runner Up in Class Meeting</li>
-    <li className="text-base md:text-lg lg:text-base">- Band Winner in Class Meeting</li>
-  </ul>
-</div>
+    {/* Experience */}
+    <div className="container-about mx-auto flex flex-col md:flex-row text-center">
+      <img
+        src="./img/bg.jpg"
+        alt="Foto Anda"
+        className="w-full md:w-1/2 lg:w-1/3 rounded-xl mx-auto mb-8 md:mb-0 md:me-5"
+      />
 
-    
+      <div className="w-full md:w-1/2 lg:w-2/3 mx-auto ps-1 text-left md:text-left " >
+        <h1 className="text-3xl md:text-4xl lg:text-5xl font-semibold font-mono text-white mb-4 md:mb-6">Experience</h1>
+        <ul className="text-lg md:text-base text-white custom-tracking space-y-3 sm:space-y-4 md:space-y-4 list-disc">
+          <li className="text-base md:text-lg lg:text-base"> 8 Times Got First Rank in Elementary School</li>
+          <li className="text-base md:text-lg lg:text-base"> Top 8 Student In Middle School</li>
+          <li className="text-base md:text-lg lg:text-base"> Runner-Up Of Genshin Impact Tournament</li>
+          <li className="text-base md:text-lg lg:text-base"> Design Winner in Class Meeting</li>
+          <li className="text-base md:text-lg lg:text-base"> Futsal Runner Up in Class Meeting</li>
+          <li className="text-base md:text-lg lg:text-base"> Band Winner in Class Meeting</li>
+        </ul>
+      </div>
+    </div>
+
   </div>
-  </div>
-
-
-  </section>
+</section>
 
 
 
 
  {/* About Section */}
  <section id="about" className="min-h-screen flex items-center justify-center">
+ <div className='container mx-auto row'>
       <div className="container-about mx-auto flex flex-col md:flex-row text-center">
         <img
           src="./img/ihya2.jpg"
@@ -218,13 +217,14 @@ const App = () => {
           className="w-full md:w-1/2 rounded-xl mx-auto mb-8 md:mb-0 me-5"
         />
         <div className="md:w-1/2 mx-auto text-left">
-          <h1 className="text-4xl md:text-5xl lg:text-6xl font-semibold font-mono text-white mb-3">
+          <h1 className="text-3xl md:text-5xl lg:text-6xl font-semibold font-mono text-white mb-3">
             Hello there!
           </h1>
-          <p className="text-xl md:text-2xl text-white">
+          <p className="text-base md:text-lg lg:text-base text-white">
             Your First Impressions: "Wow, this kid's a huge Otaku! 😄 I'm Muhammad Ihya Ulumuddin, a Software Developer. I craft digital experiences and stay updated on tech trends. When not coding, I explore anime, manga, and gaming. Technology fuels my creativity, and I love innovative projects. Connect with me for tech talks, anime suggestions, or creative collaborations. Let's make the digital world more exciting! 😄🚀🎮"
           </p>
         </div>
+      </div>
       </div>
     </section>
 
